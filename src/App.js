d78cb7b7deaf636182aec98beb49dcd362f91535
santisans123 <santitok21@gmail.com>
2022-10-19 13:06:07 +0700

    routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import GlobalStyle from './styles/GlobalStyle';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/home/Home';
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-import Register from './pages/Register';
-
+import React from "react";
+import GlobalStyle from "./styles/GlobalStyle";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Login from "./pages/Login";
+import { HelmetProvider } from "react-helmet-async";
+const helmetContext = {};
 const App = () => {
-	return (
-		<div>
-			<GlobalStyle />
-			<Router>
-				<Header />
-				<Routes>
-					<Route exact path='/' element={<Home />} />
-					<Route exact path='/register' element={<Register/>} />
- 				</Routes>
-				<Footer />
-			</Router>
-		</div>
-	);
+  return (
+    <HelmetProvider context={helmetContext}>
+      <GlobalStyle />
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </HelmetProvider>
+  );
 };
 
 export default App;
